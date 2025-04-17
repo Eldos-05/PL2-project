0a62d416057deb88dc7bde3e56875309fc5aa304
Eldos-05 <eldos.220505@gmail.com>
2025-04-17 09:23:04 +0600

    Creating entities and connection to postgresql

--- src/main/java/comsep_23/Textile_Industry_Production/entity/CustomerOrder.java
+++ src/main/java/comsep_23/Textile_Industry_Production/entity/CustomerOrder.java
@@ -1,4 +1,33 @@
 package comsep_23.Textile_Industry_Production.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer_orders")
+@Getter
+@Setter
+@NoArgsConstructor
 public class CustomerOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_login", referencedColumnName = "login")
+    private User customer;
+    @Column(nullable = false)
+    private String materialName;
+    @Column(nullable = false)
+    private Integer quantity;
+    @Column(nullable = false)
+    private LocalDateTime orderDate = LocalDateTime.now();
+
+
 }
--- src/main/java/comsep_23/Textile_Industry_Production/entity/User.java
+++ src/main/java/comsep_23/Textile_Industry_Production/entity/User.java
@@ -1,4 +1,24 @@
 package comsep_23.Textile_Industry_Production.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String login;
+    @Column(unique = true, nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String roles;
 }
